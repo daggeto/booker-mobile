f51391a4580ecb2fc64d808eeb46d251a0c36ba5
daggeto <artur@vinted.com>
2016-08-16 20:29:29 +0300

    Make gulp to compile coffee to different files

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,13 @@ var ngAnnotate = require('gulp-ng-annotate');
 var sh = require('shelljs');
 var coffee = require('gulp-coffee');
 var slim = require("gulp-slim");
+var inject = require('gulp-inject');
 
 var paths = {
   sass: ['./src/sass/**/*.scss'],
   coffee: ['./src/coffee/**/*.coffee'],
   slim: ['./src/slim/**/*.slim'],
 };
-
-var coffee_paths = {
-  root: ['./src/coffee/*.coffee'],
-  controllers: ['./src/coffee/controllers/*.coffee'],
-  services: ['./src/coffee/services/*.coffee'],
-  resources: ['./src/coffee/resources/*.coffee'],
-  components: ['./src/coffee/components/*.coffee'],
-  filters: ['./src/coffee/filters/*.coffee']
-}
 
 gulp.task('default', ['sass', 'coffee', 'slim']);
 
@@ -42,39 +34,9 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('coffee', function(done) {
-  gulp.src(coffee_paths.root)
+  gulp.src(paths.coffee)
     .pipe(coffee({bare: true})
     .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(gulp.dest('./www/js'))
-
-  gulp.src(coffee_paths.controllers)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(concat('controllers.js'))
-    .pipe(gulp.dest('./www/js'))
-
-  gulp.src(coffee_paths.services)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(concat('services.js'))
-    .pipe(gulp.dest('./www/js'))
-
-  gulp.src(coffee_paths.resources)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(concat('resources.js'))
-    .pipe(gulp.dest('./www/js'))
-
-  gulp.src(coffee_paths.filters)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(concat('filters.js'))
-    .pipe(gulp.dest('./www/js'))
-
-  gulp.src(coffee_paths.components)
-    .pipe(coffee({bare: true})
-    .on('error', gutil.log.bind(gutil, 'Coffee Error')))
-    .pipe(concat('components.js'))
     .pipe(gulp.dest('./www/js'))
     .on('end', done)
 });
@@ -85,6 +47,13 @@ gulp.task('slim', function(done){
     .pipe(slim({pretty: true, options: "attr_list_delims={'(' => ')', '[' => ']'}"}))
     .pipe(gulp.dest("./www"))
     .on('end', done);
+});
+
+gulp.task('index', ['default'], function () {
+  var target = gulp.src('./www/index.html');
+  var sources = gulp.src(['./www/js/**/*.js'], {read: false});
+
+  return target.pipe(inject(sources,{relative: true})).pipe(gulp.dest('./www'));
 });
 
 gulp.task('watch', function() {
